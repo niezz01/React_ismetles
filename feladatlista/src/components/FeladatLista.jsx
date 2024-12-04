58ae203fc6a07ac1cfdee6274eb9b31be7c43da5
niezz01 <145447146+niezz01@users.noreply.github.com>
2024-12-04 11:15:33 +0100

    csinositas

--- feladatlista/src/components/FeladatLista.jsx
+++ feladatlista/src/components/FeladatLista.jsx
@@ -3,25 +3,28 @@ import { useState } from 'react'
 const FeladatLista = () => {
 
     const [feladatok, setFeladatok] = useState([]);
-    const [ujFeladat, setUjFeladat] = useState({ szoveg: "", kesz: false});
+    const [ujFeladat, setUjFeladat] = useState({ szoveg: "", kesz: false });
 
     const ujFeladatHozzaadasa = () => {
         setFeladatok([...feladatok, ujFeladat]);
     };
 
     return (
-        <div className='text-center bg-gray-700 min-h-screen p-5'>
-            <input className='rounded mx-2' type="text" value={ujFeladat.szoveg} onChange={(e) => setUjFeladat( {...ujFeladat, szoveg: e.target.value})} />
+        <div className='text-center w-full bg-gray-700 min-h-screen p-5'>
+            <input className='rounded mx-2 px-1' type="text" value={ujFeladat.szoveg} onChange={(e) => setUjFeladat({ ...ujFeladat, szoveg: e.target.value })} />
             <button className='rounded m-2 px-2 bg-gray-50' onClick={ujFeladatHozzaadasa}>Feladat hozzáadása</button>
-            <ul>
-                {feladatok.map((feladat, index) => (
-                    <li className='p-3 space-y-2' key={index}>
-                        {feladat.szoveg} {feladat.kesz ? "Kész" : "Nincs Kész"}
-                        <input className='mx-2' value={feladat.kesz} onChange={(e) => setFeladatok(feladatok.map((f, i) => (i === index ? { ...f, kesz: e.target.checked } : f)))}type="checkbox"/>
-                        <button className='rounded mx-2 px-2 bg-gray-50' onClick={() => setFeladatok(feladatok.filter((f, i) => i !== index))}>Töröl</button>
-                    </li>
-                ))}
-            </ul>
+            <div>
+                <ul>
+                    {feladatok.map((feladat, index) => (
+                        <li className='bg-gray-400 rounded w-2/5 mx-auto my-1 p-3 space-x-32 grid grid-cols-3 items-center' key={index}>
+                            <p className='text-left w-full break-words'>{feladat.szoveg}</p>
+                            <div className='flex items-center gap-2'><input className='mx-2' value={feladat.kesz} onChange={(e) => setFeladatok(feladatok.map((f, i) => (i === index ? { ...f, kesz: e.target.checked } : f)))} type="checkbox" />
+                                <span>{feladat.kesz ? "Kész" : "Nincs Kész"}</span></div>
+                            <button className='rounded mx-2 px-2 bg-gray-50' onClick={() => setFeladatok(feladatok.filter((f, i) => i !== index))}>Töröl</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 };
